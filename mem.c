54d8bfd07e2afd52dcf91076075cd55b6b03805f
SpeekeR <DominikZappe1999@seznam.cz>
2021-11-28 12:25:25 +0100

    mem.c and mem.h is back!

---
+++ mem.c
@@ -0,0 +1,29 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include "mem.h"
+
+/** allocated memory blocks, for memory leaks checking */
+uint mem_blocks = 0;
+
+/**
+ * Function uses standard malloc and if the allocation goes well
+ * increments the memory blocks counter
+ * @param size size of wanted memory block
+ * @return pointer to wanted memory block or NULL
+ */
+void *mymalloc(size_t size) {
+    void *ptr = malloc(size);
+    if (ptr) mem_blocks++;
+    return ptr;
+}
+
+/**
+ * Function uses standard free and if the double pointer was not NULL to begin with
+ * decrements the memory blocks counter
+ * @param ptr pointer to a pointer that's to be freed
+ */
+void myfree(void **ptr) {
+    if (*ptr) mem_blocks--;
+    free(*ptr);
+    *ptr = NULL;
+} 
---
+++ mem.h
@@ -0,0 +1,13 @@
+#ifndef __MEM_H__
+#define __MEM_H__
+
+#include <stddef.h>
+
+typedef unsigned int uint;
+
+extern uint mem_blocks;
+
+void *mymalloc(size_t size);
+void myfree(void **ptr);
+
+#endif
